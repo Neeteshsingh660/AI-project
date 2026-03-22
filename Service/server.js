8594b78806bd394231e48e1d178e3fc7c9f240a3
Neeteshsingh660 <you@neeteshsingh660@gmail.com>
2026-03-22 18:51:06 +0530

    Added RemoveImage,ResumeReview

---
+++ Service/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config.js';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from "./routes/aiRoutes.js";
+import connectCloudinary from './configs/cloudinary.js';
+
+const app=express();
+await connectCloudinary() // Connect to Cloudinary before starting the server
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+app.get('/',(req,res)=>res.send('Server is live!'))
+app.use(requireAuth())
+app.use('/api/ai',aiRouter)
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>console.log(`Server is running on port `,PORT));
